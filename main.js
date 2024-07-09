45133adffd7af1c397c9b66f6cf34a94ad1b1989
victoryalhj <victoryalhj@gmail.com>
2024-07-09 10:10:52 +0100

    240709

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 // const API_KEY = `d787306ca4424a128d6c0ff4f0061698`
-let news = [];
+let pageSize = 10;
+let page = 2;
+let category = "";
+let keyword = "";
 
+let newsList = [];
 const getLatestNews = async () => {
-  let url = new URL(`https://victoria-news.netlify.app/top-headlines`);
-  let response = await fetch(url);
-  let data = await response.json();
-  news = data.articles;
-    console.log("ddd", news);
+  const url = new URL(
+    `https://victoria-news.netlify.app/top-headlines?country=kr&pageSize=${pageSize}&page=${page}${category}${keyword}`
+    ); 
+
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log("ddd",newsList);
+}
+
+//ui그려줌, TODO-LIST for문 ,ES6 array함수, join
+const render = () => {
+  const newsHTML = newsList.map(
+    (news) => `<div class="row news">
+         <div class="col-lg-4">
+          <img class="news-img-size" src=${news.urlToImage} alt="">
+        </div>
+        <div class="col-lg-8">
+          <h2>${news.title}</h2>
+          <p>${news.description}</p>
+          <div>
+            ${news.source.name} * ${news.publishedAt}
+          </div>
+        </div>
+      </div>`
+    ).join('');
+    console.log("html", newsHTML);
+
+  document.getElementById('news-board').innerHTML = newsHTML
 }
 
 getLatestNews();

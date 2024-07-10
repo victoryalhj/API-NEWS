a98fda64d44004f332e0c46b174531f6fd7cdc29
victoryalhj <victoryalhj@gmail.com>
2024-07-11 00:17:56 +0100

    20240711

--- main.js
+++ main.js
@@ -44,3 +44,22 @@ const render = () => {
 }
 
 getLatestNews();
+
+
+//search
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', 'Search');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});

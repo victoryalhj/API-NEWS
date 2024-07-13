6ca3d65850ea89f0b95e7eca185eb2cc44c240fa
victoryalhj <victoryalhj@gmail.com>
2024-07-13 21:46:38 +0100

    Changed paginationRender

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ const openSearchBox = () => {
   }
 }
 
-//Side-menu
+//SideMenu
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
 };
@@ -87,7 +87,7 @@ const closeNav = () => {
 }
 
 
-//ui그려줌, TODO-LIST for문 ,ES6 array함수, join
+//Render
 const render = () => {
   const newsHTML = newsList.map(
     (news) => `<div class="row news">
@@ -114,8 +114,9 @@ const errorRender = (errorMessage) => {
   document.getElementById("news-board").innerHTML = errorHTML
 };
 
-
+//Page
 const paginationRender = () => {
+  let paginationHTML=``;
   const totalPages = Math.ceil(totalResults/pageSize)
   let pageGroup = Math.ceil(page/groupSize);
   let lastPage = pageGroup * groupSize;
@@ -123,24 +124,36 @@ const paginationRender = () => {
     lastPage = totalPages
   }
   const firstPage = lastPage - (groupSize-1)<=0? 1: lastPage - (groupSize - 1);
-  
-  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`;
+  let last = pageGroup * 5;
+    if(last > totalPages) {
+      last = totalPages;
+    }
+  let first = last -4 <= 0 ? 1 : last -4;
 
-  for(let i=firstPage; i<=lastPage; i++){
-    paginationHTML+=` <li class="page-item ${i === page? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  if(page > 1) {
+    paginationHTML = `<li class="page-item" onclick="moveToPage(1)">
+                          <a class="page-link" href="#">&lt;&lt;</a>
+                        </li>
+                        <li class="page-item" onclick="moveToPage(${page-1})">
+                          <a class="page-link" href="#">&lt;</a>
+                        </li>`;
   }
-  paginationHTML+=`<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`
+  for(let i=firstPage; i<=lastPage; i++){
+    paginationHTML+=` <li class="page-item ${i === page? "active" : ""}" onclick="moveToPage(${i})">
+                        <a class="page-link">${i}</a> 
+                      </li>`;
+  }
+  if(page < totalPages) {
+  paginationHTML+=`<li class="page-item" onclick="moveToPage(${page+1})">
+                    <a class="page-link" href="#">&gt;</a>
+                  </li>
+                  <li class="page-item" onclick="moveToPage(${totalPages})">
+                    <a class="page-link" href="#">&gt;&gt;</a>
+                  </li>`;
+  }
   document.querySelector(".pagination").innerHTML = paginationHTML
 
-//   <nav aria-label="Page navigation example">
-//   <ul class="pagination">
-//     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-//     <li class="page-item"><a class="page-link" href="#">1</a></li>
-//     <li class="page-item"><a class="page-link" href="#">2</a></li>
-//     <li class="page-item"><a class="page-link" href="#">3</a></li>
-//     <li class="page-item"><a class="page-link" href="#">Next</a></li>
-//   </ul>
-// </nav>
+
 }
 
 const moveToPage = (pageNum) => {
